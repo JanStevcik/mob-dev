50839652575b051565713286b6550bfd5c3f499f
Ján Števčík <jan.stevcik@student.tuke.sk>
2021-01-23 22:13:55 +0100

    move functionality to world class

--- index.ts
+++ index.ts
@@ -1,52 +1,46 @@
 import {Man} from './src/man'
 import {Woman} from './src/woman'
+import {Delegate} from "./src/delegate"
 
 // @ts-ignore
-declare function require(name:string);
-const process = require('process');
-
-//2. task
-import {Delegate} from "./src/delegate";
-const delegate = new Delegate()
-
-/** BEFORE YOU START
- *  Please create github repo where you will store your solution
- *  After every task please commit and push changes
- * */
-
-const sebastian = new Man(delegate,'Sebastian')
-const natalia = new Woman(delegate,'Natalia')
-
-//1.  Expected output: 4:13:02 PM
-const getDate = () =>{
-    let date = new Date().toLocaleString('en-US');
-    return date.substr(date.indexOf(' ')+1);
-}
-
-process.stdout.write(getDate() + '\n');
-sebastian.say('Hello my name is Sebastian')
-
-process.stdout.write(getDate() + '\n');
-natalia.say('Hello my name is Natalia')
-
-process.stdout.write(getDate() + '\n');
-sebastian.say('Are you free?')
-
-process.stdout.write(getDate() + '\n');
-natalia.say('No I have a boyfriend')
-
-// 2nd task output
-console.log(delegate.getDelegates());
-
-/** 3.
-
- * */
-
+declare function require(name:string)
+const process = require('process')
 
 class World {
+    private delegate = new Delegate()
+    private natalia = new Woman(this.delegate, 'Natalia')
+    private sebastian = new Man(this.delegate, 'Sebastian')
+
     constructor() {
     }
+
+    private getDate = () => {
+        let date = new Date().toLocaleString('en-US')
+        return date.substr(date.indexOf(' ')+1)
+    }
+
+    public communicate(){
+        process.stdout.write(this.getDate() + '\n')
+        this.sebastian.say('Hello my name is Sebastian')
+
+        process.stdout.write(this.getDate() + '\n')
+        this.natalia.say('Hello my name is Natalia')
+
+        process.stdout.write(this.getDate() + '\n')
+        this.sebastian.say('Are you free?')
+
+        process.stdout.write(this.getDate() + '\n')
+        this.natalia.say('No I have a boyfriend')
+    }
+
+    public printLogs(){
+        console.log(this.delegate.getDelegates())
+    }
 }
+
+const world = new World()
+world.communicate()
+world.printLogs()
 
 
 
